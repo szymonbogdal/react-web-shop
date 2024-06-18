ceeaf00f6e37f22bc1f0f430dc43653a849876f5
Szymon Bogdał <bogdal.symon@gmail.com>
2024-06-19 00:31:57 +0200

    cart styles

--- src/components/Navbar/Cart.jsx
+++ src/components/Navbar/Cart.jsx
@@ -6,7 +6,21 @@ function Cart(){
     
     return(
         <div className="cart-container">
-            {state.items?.map(item => <CartItem item={item} key={item.id}/>)}             
+            <h1 className="cart-container__heading">Your cart</h1>
+            <hr></hr>
+            <div className="cart-container__body">
+                {state.items.length>0?
+                state.items.map(item => <CartItem item={item} key={item.id}/>)
+                :
+                <p className="cart-container__body-empty-msg">Your cart is empty</p>    
+                }
+            </div>    
+            <hr></hr>
+            <div className="cart-container__summary">
+                <p className="cart-container_summary-total">Total: </p>
+                <p className="cart-container_summary-price">245.99$</p>
+            </div>
+            <button className="cart-container__checkout">Checkout</button>             
         </div>
     )
 }
--- src/components/Navbar/CartItem.jsx
+++ src/components/Navbar/CartItem.jsx
@@ -1,15 +1,22 @@
 import { useCartContext } from "../../context/CartContext";
 
 function CartItem(props){
-    const {id, image, title, price, category, qty } = props.item;
+    const {id, image, title, price, qty } = props.item;
     const {removeItem, incrementQty, decrementQty} = useCartContext();
     return(
-        <>
-            <h1>{id} {qty}</h1>
-            <button onClick={()=>removeItem(id)}>Remove</button>
-            <button onClick={()=>incrementQty(id)}>Increment</button>
-            <button onClick={()=>decrementQty(id)}>Decrement</button>
-        </>
+        <div className="cart-item">
+            <img className="cart-item__img" src={image} alt={title} />
+            <div className="cart-item__body">
+                <h4 className="cart-item__body-heading">{title}</h4>
+                <div className="cart-item__body-action">
+                    <button className="cart-item__body-action-btn" onClick={()=>decrementQty(id)} disabled={qty<=1}>-</button>
+                    <p className="cart-item__body-action-qty">{qty}</p>
+                    <button className="cart-item__body-action-btn" onClick={()=>incrementQty(id)} disabled={qty>=9}>+</button>
+                </div>
+            </div>
+            <p className="cart-item__price">{(price*qty).toFixed(2)}$</p>
+            <button className="cart-item__remove" onClick={()=>removeItem(id)}>&#x2715;</button>
+        </div>
     )
 }
 
