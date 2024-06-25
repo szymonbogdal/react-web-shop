f0f8efb4e1f9cc1f6bfbd1a97c7327d709198e08
Szymon Bogdał <bogdal.symon@gmail.com>
2024-06-25 23:27:11 +0200

    link to single product page from products list

--- src/pages/Products/Product.jsx
+++ src/pages/Products/Product.jsx
@@ -1,15 +1,19 @@
 import { useCartContext } from "../../context/CartContext";
 import shortenText from "../../utils/shortenText";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Link } from "react-router-dom";
 
 function Product(props){
+    
     const {id, image, title, price, category, rating} = props.product;
     const {addItem, state} = useCartContext();
     return(
         <div className="product-item">
-            <img className="product-item__img" src={image} alt={title} />
-            <p className="product-item__category">{category}</p>
-            <p className="product-item__title">{shortenText(title, 40)}</p>
+             <Link to={`product/${id}`} className="product-item__link">
+                <img className="product-item__img" src={image} alt={title} />
+                <p className="product-item__category">{category}</p>
+                <p className="product-item__title">{shortenText(title, 40)}</p>
+            </Link>
             <div className="product-item__info">
                 <p className="product-item__info-price">{price}$</p>
                 <p className="product-item__info-rating">{rating.rate}<FontAwesomeIcon icon="fa-solid fa-star" className="product-item__info-star"/></p>
