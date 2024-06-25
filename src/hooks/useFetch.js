fbff319ee8efc6ca4474bda162a2be0371758573
Szymon Bogdał <bogdal.symon@gmail.com>
2024-06-25 19:19:55 +0200

    useFetch update

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -3,14 +3,16 @@ import { useState, useEffect } from "react";
 const useFetch = (params) =>{
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(false);
-    const [data, setData] = useState();
+    const [data, setData] = useState(null);
 
     useEffect(()=>{
-        const fetchData = async () =>{
+        async function fetchData(){
+            setLoading(true);
+            setError(false);
             try {
                 let url = 'https://fakestoreapi.com/products';
                 if(params){
-                    url += `/category/${params}`
+                    url += "/"+params;
                 }
                 const request = await fetch(url);
                 if(!request.ok){
