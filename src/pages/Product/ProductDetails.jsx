b67994d78dca15368809114c5c513a1fb1192c8f
Szymon Bogdał <bogdal.symon@gmail.com>
2024-06-27 20:50:41 +0200

    product page update

--- src/pages/Product/ProductDetails.jsx
+++ src/pages/Product/ProductDetails.jsx
@@ -10,17 +10,19 @@ function ProductDetails(props){
             <img className="product__img" src={image} alt={title} />
             <div className="product__body">
                 <p className="product__body-title">{title}</p>
-                <p className="product__body-category">{category}</p>
+                <p className="product__body-category">{category} </p>
+                <p className="product__body-price">{price}$</p>
+                <p className="product__body-rating">{rating.rate}<FontAwesomeIcon icon="fa-solid fa-star" className="product__body-rating-star"/></p>
+                <p className="product__body-description-header">Description</p>
+                <hr></hr>
                 <p className="product__body-description">{description}</p>
-                <div className="product__body-info">
-                    <p className="product__body-info-price">{price}$</p>
-                    <p className="product__body-info-rating">{rating.rate}<FontAwesomeIcon icon="fa-solid fa-star" className="product__body-info-star"/></p>
-                </div>
+                
                 {state.items.find(item=>item.id === id)?
                 <button className="product__body-btn product__body-btn--added" disabled>Item in cart</button>
                 :
                 <button className="product__body-btn product__body-btn--add" onClick={()=>addItem(props.product)}>Add to cart</button>
                 }
+                <button className="product__body-btn product__body-btn--buy">Buy now</button>
             </div>
         </div>
     )
