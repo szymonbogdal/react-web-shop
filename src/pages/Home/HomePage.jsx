78dfc8aa90c16cfe79749c3a37d2398810b8ab73
Szymon Bogdał <bogdal.symon@gmail.com>
2024-07-08 21:20:52 +0200

    HomePage replaced lorem ipsum with detailed description

--- src/pages/Home/HomePage.jsx
+++ src/pages/Home/HomePage.jsx
@@ -9,9 +9,9 @@ function HomePage(props){
                     <p className="home__text-body-header">New Collection:</p>
                     <p className="home__text-body-title">Men's clothing</p>
                     <p className="home__text-body-info">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                        Maiores laborum laboriosam harum voluptatum exercitationem quis? Exercitationem assumenda ab, 
-                        autem distinctio dolorem, nulla suscipit repellat facilis animi mollitia est maxime deleniti!
+                        Discover our latest collection of men's clothing. 
+                        Crafted with premium materials and attention to detail, each piece is designed to provide both comfort and style. 
+                        Upgrade your wardrobe with our new arrivals and experience the perfect blend of fashion and functionality.
                     </p>
                     <div className="home__body-links">
                         <Link to={"/products/category/men's%20clothing"} className="home__body-link home__body-link--category">Go to category</Link>
